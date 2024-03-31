4c4af01b7f72f27c6671498f72d7fe355f803f4e
Premal <premal.kamani@yudiz.com>
2024-03-31 15:19:25 +0530

    update settings

--- writenowio/settings.py
+++ writenowio/settings.py
@@ -126,6 +126,7 @@ load_dotenv()
 # }
 
 # FORCE_SCRIPT_NAME = '/writenowio'
+
 # STATIC_URL = FORCE_SCRIPT_NAME + '/static/'
 # MEDIA_URL = FORCE_SCRIPT_NAME + '/media/
 
@@ -167,7 +168,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATIC_ROOT = BASE_DIR / "staticfiles" 
+# STATIC_ROOT = BASE_DIR / "staticfiles" 
 
 
 STATICFILES_DIRS = [
